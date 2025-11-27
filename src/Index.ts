a10a02ff7d4192d3dd0047fb9d73ba4a9bd7334d
LoveraA-droid <20231669@aloe.ulima.edu.pe>
2025-11-26 23:25:55 -0500

    llamada a logros y correcciones menores

--- src/Index.ts
+++ src/Index.ts
@@ -153,7 +153,18 @@ app.post("/Eliminar_Suscripcion", async (req : Request, resp : Response) => {
 
 app.post("/Actualizar_NivelViewer", async (req : Request, resp : Response) => {
     try {
-
+      const { ID_ChatViewer, ID_chatStreamer, NuevoNivel } = req.body
+      const NivelChat = await prisma.chatStreamer.update({
+        where: {
+          ID_Streamer_ID_Viewer: {
+            ID_Streamer: Number(ID_chatStreamer),
+            ID_Viewer: Number(ID_ChatViewer)
+          }
+        },
+        data: {
+          NivelViewer: Number(NuevoNivel)
+        }
+      })
     } catch (err){
       console.error(err)
       resp.status(400).json({ error: "Error actualizando nivel viewer" })
@@ -162,7 +173,15 @@ app.post("/Actualizar_NivelViewer", async (req : Request, resp : Response) => {
 
 app.post("/Actualizar_NivelStreams", async (req : Request, resp : Response) => {
     try {
-
+      const { ID_Usuario, NuevoNivel } = req.body
+      const NivelStreams = await prisma.usuario.update({
+        where: {
+          ID: Number(ID_Usuario)
+        },
+        data: {
+          NivelStreams: Number(NuevoNivel)
+        }
+      })
     } catch (err){
       console.error(err)
       resp.status(400).json({ error: "Error actualizando nivel streams" })
@@ -182,9 +201,30 @@ app.get("/UsuariosRanking", async (req : Request, resp : Response) => {
           ImagenPerfil: true,
         }
       })
+      resp.status(200).json(Usuarios)
     }catch (err){
       console.error(err)
       resp.status(400).json({ error: "Error listando el ranking" })
+    }
+})
+
+app.get("LogrosUsuario", async (req : Request, resp : Response) => {
+    try{
+      const { ID_Usuario } = req.body
+      const Logros = await prisma.logros.findMany({
+        where:{
+          ID_Usuario: Number(ID_Usuario)
+        },
+        select: {
+          Nombre: true,
+          Puntaje: true,
+          Completado: true
+        }
+      })
+      resp.status(200).json(Logros)
+    }catch (err){
+      console.error(err)
+      resp.status(400).json({ error: "Error obteniendo logros del usuario" })
     }
 })
 
