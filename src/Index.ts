0bc62989e80a0cd30f0aa3a953763ab1a77fb63a
LoveraA-droid <20231669@aloe.ulima.edu.pe>
2025-11-29 22:10:03 -0500

    "aaaaaaaaaaaaaaaaaaaaaa"

--- src/Index.ts
+++ src/Index.ts
@@ -53,6 +53,7 @@ app.post("/Validar_Usuario", async (req : Request, resp : Response) => {
           NombreUsuario, Contraseña
         },
         select: {
+          ID: true,
           NombreUsuario: true,
           HorasTransmision: true,
           Monedas: true,
@@ -445,7 +446,7 @@ app.get("/datos_Stream", async (req : Request, resp : Response) => {
 
 app.post("/Crear_VIdeo", async (req : Request, resp : Response) => {
     try {
-      const { titulo, url, estado, categoriaDeVideo, ID_Usuario } = req.body
+      const { titulo, url, duracion, estado, categoriaDeVideo, ID_Usuario } = req.body
       const ListaCategorias = categoriaDeVideo.join(", ");
       const video = await prisma.video.create({
         data: {
@@ -647,48 +648,6 @@ app.get("/regalos", async (req: Request, resp: Response) => {
         resp.status(500).json({ error: "Hubo un error al obtener la lista" })
     }
 })
-
-app.get("/videos/buscar", async (req: Request, resp: Response) => {
-   
-    const busqueda = req.query.q as string;
-
-    if (!busqueda) {
-        return resp.status(400).json({ 
-            error: "Escribe algo para buscar. Ejemplo: /videos/buscar?q=minecraft" 
-        });
-    }
-
-    try {
-        const videosEncontrados = await prisma.video.findMany({
-            where: {
-                OR: [
-                   
-                    { 
-                        Titulo: { contains: busqueda, mode: 'insensitive' } 
-                    },
-                    { 
-                        CategoriaDeVideo: { contains: busqueda, mode: 'insensitive' } 
-                    },
-                    { 
-                        usuario: { 
-                            NombreUsuario: { contains: busqueda, mode: 'insensitive' }
-                        }
-                    }
-                ]
-            },
-        
-            include: {
-                usuario: true 
-            }
-        });
-
-        resp.status(200).json(videosEncontrados);
-
-    } catch (error) {
-        console.error("Error buscando videos:", error);
-        resp.status(500).json({ error: "Error al realizar la búsqueda" });
-    }
-});
 
 //ENDPOINT PARA LA TRANSMISIÓN EN VIVO
 app.get("/api/live-url", (req: Request, res: Response) => {
